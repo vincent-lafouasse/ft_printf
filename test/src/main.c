d70d86908aed5a1b860780a89c9eb9a5f9af52d8
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-01-19 22:42:59 +0100

    rename

--- test/src/main.c
+++ test/src/main.c
@@ -1,14 +1,13 @@
-#include "my_lib.h"
 #include "unity.h"
 #include "unity_fixture.h"
 
 static void runAllTests()
 {
-    RUN_TEST_GROUP(Module);
+    RUN_TEST_GROUP(ReturnValue);
 }
 
 int main(int ac, const char** av)
 {
-    UnityBegin("ft_rintf");
+    UnityBegin("ft_printf");
     return (UnityMain(ac, av, runAllTests));
 }
