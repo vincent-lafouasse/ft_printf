59f396b885486444a8fbabdf3c4e95c9378bf47d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-02-01 19:40:55 +0100

    minor refactor

--- test/src/lexeme_list.c
+++ test/src/lexeme_list.c
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 07:39:17 by poss              #+#    #+#             */
-/*   Updated: 2024/01/24 07:54:00 by poss             ###   ########.fr       */
+/*   Updated: 2024/02/01 19:39:09 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,49 @@
 #include "unity_fixture.h"
 
 #include <stdio.h>
+#include <string.h>
 
 TEST_GROUP(LexemeList);
 TEST_SETUP(LexemeList) {}
 TEST_TEAR_DOWN(LexemeList) {}
+
+static void assert_lexeme(t_lexeme lexeme, const char* expected)
+{
+	uint64_t size = lexeme.end - lexeme.start;
+
+	if (size < 0)
+	{
+		FAIL("invalid lexeme, end is before start");
+		return;
+	}
+
+	if (size > strlen(expected))
+	{
+		FAIL("lexeme is longer than expected");
+		return;
+	}
+
+	for (char* it = (char*)lexeme.start; it != lexeme.end; it++, expected++)
+	{
+		TEST_ASSERT_EQUAL(*it, *expected);
+	}
+}
 
 TEST(LexemeList, NewNode)
 {
 	const char* src = "abcdefgh";
 
 	t_lexeme_node* node = lexeme_node_new(src, src + 3);
-	t_lexeme lexeme = node->lexeme;
 
-    TEST_ASSERT_EQUAL(lexeme.start, src);
-    TEST_ASSERT_EQUAL(lexeme.end, src + 3);
+	assert_lexeme(node->lexeme, "abc");
     TEST_ASSERT_EQUAL(node->next, NULL);
 }
 
 TEST(LexemeList, NewList)
 {
 	t_lexeme_list list = lexeme_list_new();
-	TEST_ASSERT_EQUAL(list.head, list.tail);
 	TEST_ASSERT_EQUAL(list.head, NULL);
+	TEST_ASSERT_EQUAL(list.tail, NULL);
 }
 
 TEST(LexemeList, ListPush)
@@ -47,17 +68,14 @@ TEST(LexemeList, ListPush)
 	lexeme_list_push(&list, src, src + 4);
 	TEST_ASSERT_NOT_EQUAL(list.head, NULL);
 	TEST_ASSERT_EQUAL(list.head, list.tail);
-	TEST_ASSERT_EQUAL(list.head->lexeme.start, src);
-	TEST_ASSERT_EQUAL(list.head->lexeme.end, src + 4);
+	assert_lexeme(list.head->lexeme, "abcd");
 
 
 	lexeme_list_push(&list, src + 3, src + 7);
 	TEST_ASSERT_NOT_EQUAL(list.head, NULL);
 	TEST_ASSERT_NOT_EQUAL(list.head, list.tail);
-	TEST_ASSERT_EQUAL(list.head->lexeme.start, src);
-	TEST_ASSERT_EQUAL(list.head->lexeme.end, src + 4);
-	TEST_ASSERT_EQUAL(list.tail->lexeme.start, src + 3);
-	TEST_ASSERT_EQUAL(list.tail->lexeme.end, src + 7);
+	assert_lexeme(list.head->lexeme, "abcd");
+	assert_lexeme(list.tail->lexeme, "defg");
 }
 
 TEST_GROUP_RUNNER(LexemeList)
