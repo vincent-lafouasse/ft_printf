66fcd7468e065601dbb92d5ec27e05441a2bd9df
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-02-02 16:52:30 +0100

    minor refactor

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -6,13 +6,13 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 22:36:42 by poss              #+#    #+#             */
-/*   Updated: 2024/01/19 22:38:05 by poss             ###   ########.fr       */
+/*   Updated: 2024/02/02 16:52:22 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-int	ft_printf(const char *, ...);
+int	ft_printf(const char *format, ...);
 
 #endif
