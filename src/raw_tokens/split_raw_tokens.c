0b83a2449b801d96ef5a18d2b2e6a4fb8b0e3d7d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-03-12 22:08:19 +0100

    format

--- src/raw_tokens/split_raw_tokens.c
+++ src/raw_tokens/split_raw_tokens.c
@@ -16,34 +16,34 @@
 
 #define CONVERSION_SPECIFIERS "cspdiuxX%"
 
-static bool is_conversion_specifier(char c)
+static bool	is_conversion_specifier(char c)
 {
-    return (strchr(CONVERSION_SPECIFIERS, c));
+	return (strchr(CONVERSION_SPECIFIERS, c));
 }
 
-t_substr_deque split_raw_tokens(const char* format)
+t_substr_deque	split_raw_tokens(const char *format)
 {
-    t_substr_deque raw_tokens;
-    const char* start;
+	t_substr_deque	raw_tokens;
+	const char		*start;
 
-    raw_tokens = substr_deque_new();
-    while (*format)
-    {
-        start = format;
-        if (*format != '%')
-        {
-            while (*format && *format != '%')
-                format++;
-            substr_deque_push(&raw_tokens, start, format - start);
-        }
-        else
-        {
-            format++;
-            while (!is_conversion_specifier(*format))
-                format++;
-            substr_deque_push(&raw_tokens, start, format + 1 - start);
-            format++;
-        }
-    }
-    return (raw_tokens);
+	raw_tokens = substr_deque_new();
+	while (*format)
+	{
+		start = format;
+		if (*format != '%')
+		{
+			while (*format && *format != '%')
+				format++;
+			substr_deque_push(&raw_tokens, start, format - start);
+		}
+		else
+		{
+			format++;
+			while (!is_conversion_specifier(*format))
+				format++;
+			substr_deque_push(&raw_tokens, start, format + 1 - start);
+			format++;
+		}
+	}
+	return (raw_tokens);
 }
--- src/substring/t_substr_deque.c
+++ src/substring/t_substr_deque.c
@@ -14,38 +14,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-t_substr_deque substr_deque_new(void)
+t_substr_deque	substr_deque_new(void)
 {
-    t_substr_deque out;
+	t_substr_deque	out;
 
-    out.head = NULL;
-    out.tail = NULL;
-    return (out);
+	out.head = NULL;
+	out.tail = NULL;
+	return (out);
 }
 
-t_substr_list* substr_list_new(const char* start, size_t len)
+t_substr_list	*substr_list_new(const char *start, size_t len)
 {
-    t_substr_list* out;
-    t_substr substr;
+	t_substr_list	*out;
+	t_substr		substr;
 
-    out = malloc(sizeof(*out));
-    substr.start = start;
-    memcpy((void*)&(substr.len), &len, sizeof(len));
-    memcpy((void*)&(out->substr), &substr, sizeof(substr));
-    out->next = NULL;
-    return (out);
+	out = malloc(sizeof(*out));
+	substr.start = start;
+	memcpy((void *)&(substr.len), &len, sizeof(len));
+	memcpy((void *)&(out->substr), &substr, sizeof(substr));
+	out->next = NULL;
+	return (out);
 }
 
-void substr_deque_push(t_substr_deque* l, const char* start, const size_t len)
+void	substr_deque_push(t_substr_deque *l, const char *start,
+		const size_t len)
 {
-    if (l->head == NULL)
-    {
-        l->head = substr_list_new(start, len);
-        l->tail = l->head;
-        return;
-    }
-    l->tail->next = substr_list_new(start, len);
-    l->tail = l->tail->next;
+	if (l->head == NULL)
+	{
+		l->head = substr_list_new(start, len);
+		l->tail = l->head;
+		return ;
+	}
+	l->tail->next = substr_list_new(start, len);
+	l->tail = l->tail->next;
 }
 
-void substr_deque_clear(t_substr_deque* l);
+void			substr_deque_clear(t_substr_deque *l);
