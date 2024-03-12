ab0d1ede348bf7aa670b404cf59a8ba7c922761e
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-03-12 21:15:22 +0100

    refactor

--- src/substring/t_substr.h
+++ src/substring/t_substr.h
@@ -6,17 +6,19 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 07:11:18 by poss              #+#    #+#             */
-/*   Updated: 2024/03/12 19:43:35 by poss             ###   ########.fr       */
+/*   Updated: 2024/03/12 21:09:45 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef T_SUBSTR_H
 #define T_SUBSTR_H
 
+#include <stddef.h>
+
 typedef struct s_substr
 {
     const char* start;
-    const char* end;
+    const size_t len;
 } t_substr;
 
 #endif
