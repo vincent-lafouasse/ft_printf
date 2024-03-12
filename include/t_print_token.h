4a165ee86954953c04f9ea1f2bf0de1bd96db24d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-03-12 18:46:29 +0100

    token sketch

---
+++ include/t_print_token.h
@@ -0,0 +1,55 @@
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef struct
+{
+    const char* start;
+    const char* end;
+} t_literal_token;
+
+// ----------------------------------------------------------------------------
+
+typedef enum
+{
+	SIGNED,
+    UNSIGNED,
+    STRING,
+    CHAR,
+} t_type_specifier;
+
+typedef enum
+{
+    NONE,
+    LONG,
+} t_type_modifier;
+
+typedef struct
+{
+    const t_type_specifier type;
+    const t_type_modifier type_modifier;
+    const size_t minimum_field_width;
+    const size_t precision;
+    const bool pad_left;
+    const char pad_with;
+    const bool show_sign;
+} t_format_token;
+
+// ----------------------------------------------------------------------------
+
+typedef enum 
+{
+    LITERAL,
+    FORMAT,
+} t_token_type;
+
+typedef union
+{
+    t_literal_token literal;
+    t_format_token format;
+} t_any_token;
+
+typedef struct
+{
+    t_token_type type;
+    t_any_token data;
+} t_token;
