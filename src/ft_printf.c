22d38459b4030f2d4bb3f46a33a4c0a93b330ca8
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-03-19 17:40:34 +0100

    sketch

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,21 +6,22 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 22:38:17 by poss              #+#    #+#             */
-/*   Updated: 2024/03/19 17:23:27 by poss             ###   ########.fr       */
+/*   Updated: 2024/03/19 17:40:15 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "raw_tokens/split_raw_tokens.h"
 #include "substring/t_substr_deque.h"
+#include "tokens/t_tokens.h"
 
 int	ft_printf(const char *format, ...)
 {
 	t_substr_deque raw_tokens = split_raw_tokens(format);
-	t_substr current_raw_token;
+	t_token current_token;
 
 	while (!substr_deque_isempty(raw_tokens))
 	{
-		current_raw_token = substr_deque_pop_front(&raw_tokens);
+		current_token = parse_raw_token(substr_deque_pop_front(&raw_tokens));
 	}
 
 	return (0);
