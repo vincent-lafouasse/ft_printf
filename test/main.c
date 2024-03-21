868f5807da9be71a81b21e4dc2127b4a4476d775
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-03-21 15:10:29 +0100

    new manual test

--- test/main.c
+++ test/main.c
@@ -4,7 +4,7 @@
 int main(void)
 {
 	printf("output start:\n");
-	ft_printf("hello%i %c%c%c", 69, '4', '2', '0');
-	//printf("lol %c %s lol", '0', (char*)NULL);
+	//ft_printf("hello%i %c%c%c", 69, '4', '2', '0');
+	printf("lol %x %s lol", 0, (char*)NULL);
 	printf("\noutput end");
 }
