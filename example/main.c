5affa1a5af11e7862ef54b9569e25be8d2168599
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-07 20:41:33 +0200

    more tests

--- example/main.c
+++ example/main.c
@@ -5,5 +5,9 @@ int main(void)
 {
 	ft_printf("hel");
 	ft_printf("");
-	ft_printf("lo\n");
+	ft_printf("lo\n\n");
+
+	ft_printf("Voici %s: %d\n", "int max", INT_MAX);
+	const char* int_min = "int min";
+	ft_printf("Et voici %s: %d\n", int_min, INT_MIN);
 }
