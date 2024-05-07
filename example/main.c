f7938312d3bfe111e64e5cb249306a0ba2c02a03
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-07 20:47:02 +0200

    update tests

--- example/main.c
+++ example/main.c
@@ -1,5 +1,6 @@
 #include "ft_printf.h"
 #include <limits.h>
+#include <stdio.h>
 
 int main(void)
 {
@@ -8,6 +9,12 @@ int main(void)
 	ft_printf("lo\n\n");
 
 	ft_printf("Voici %s: %d\n", "int max", INT_MAX);
+	printf("On devrait voir %d\n", INT_MAX);
 	const char* int_min = "int min";
 	ft_printf("Et voici %s: %d\n", int_min, INT_MIN);
+	printf("On devrait voir %d\n", INT_MIN);
+
+	ft_printf("%x\n", 0xBABECAFE);
+	ft_printf("%X\n", 0xBABECAFE);
+	ft_printf("%p\n", 0xBABECAFE);
 }
