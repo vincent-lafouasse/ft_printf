0134b42b079c1540df7d80999d375c0362b86c53
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-09 22:14:01 +0200

    format

--- example/main.c
+++ example/main.c
@@ -1,22 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/09 22:13:00 by poss              #+#    #+#             */
+/*   Updated: 2024/05/09 22:13:49 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 #include <limits.h>
 #include <stdio.h>
 
-int main(void)
+int	main(void)
 {
-	ft_printf("%p\n", (void*)0xBABECAFE);
-	/*
+	const char	*int_min = "int min";
+
 	ft_printf("hel");
 	ft_printf("");
 	ft_printf("lo\n\n");
-
 	ft_printf("Voici %s: %d\n", "int max", INT_MAX);
 	printf("On devrait voir %d\n", INT_MAX);
-	const char* int_min = "int min";
 	ft_printf("Et voici %s: %d\n", int_min, INT_MIN);
 	printf("On devrait voir %d\n", INT_MIN);
-
 	ft_printf("%x\n", 0xBABECAFE);
 	ft_printf("%X\n", 0xBABECAFE);
-	*/
+	ft_printf("%p\n", (void *)0xBABECAFE);
 }
